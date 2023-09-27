cbbc8f9699750dd206588ddd0c14205a37a072cd
Robin Berjon <robin.berjon@nytimes.com>
2023-09-27 18:15:18 +0000

    'it works with mkdir and save'

--- experiments/trying-wnfs.js
+++ experiments/trying-wnfs.js
@@ -2,8 +2,8 @@
 
 import Store from '../lib/store.js';
 
-// const s = Store.createFromCID('/Users/robin/Code/darobin/polypod-wnfs/scratch/cid-store', 'bafkr4iagsmbphhyzkjhkd3ufr462y6eufalfhqgj4whonw2kw5av3waote');
 const s = Store.createEmpty('/Users/robin/Code/darobin/polypod-wnfs/scratch/cid-store');
 await s.mkdir(['robin', 'notes']);
 await s.save(['robin', 'notes', 'poem.txt'], 'time will say nothing but I told you so');
-console.warn(`Done!`);
+const cid = await s.put();
+console.warn(`Done: ${cid.toString()}!`);
