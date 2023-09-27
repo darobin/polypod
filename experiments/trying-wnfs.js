bf0ab8add536ec6966ea9d5e9d642e5ddfa68309
Robin Berjon <robin.berjon@nytimes.com>
2023-09-27 17:07:49 +0000

    'better'

--- experiments/trying-wnfs.js
+++ experiments/trying-wnfs.js
@@ -2,6 +2,7 @@
 
 import Store from '../lib/store.js';
 
+// const s = Store.createFromCID('/Users/robin/Code/darobin/polypod-wnfs/scratch/cid-store', 'bafkr4iagsmbphhyzkjhkd3ufr462y6eufalfhqgj4whonw2kw5av3waote');
 const s = Store.createEmpty('/Users/robin/Code/darobin/polypod-wnfs/scratch/cid-store');
 await s.mkdir(['robin', 'notes']);
 await s.save(['robin', 'notes', 'poem.txt'], 'time will say nothing but I told you so');
