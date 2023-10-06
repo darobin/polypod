493c938be385c7e7c8984ee5a4a7428f4a01dc0f
Robin Berjon <robin.berjon@nytimes.com>
2023-10-06 15:32:50 -0400

    'moving towards creating an account'

--- experiments/create-account.js
+++ experiments/create-account.js
@@ -1,14 +1,17 @@
 
 import API from '@atproto/api';
+import { getInvite } from './shared.js';
 import { pdsURL, accounts } from './data.js';
 
 const { BskyAgent } = API;
 const agent = new BskyAgent({ service: pdsURL });
 
+const inviteCode = await getInvite();
+
 // create a new account on the server
 await agent.createAccount({
   ...accounts.kitsune,
-  inviteCode: 'some-code-12345-abcde',
+  inviteCode,
 });
 
 console.warn(`ok…`);
--- experiments/get-invite.js
+++ experiments/get-invite.js
@@ -1,24 +1,4 @@
 
-// import API from '@atproto/api';
-import { pdsURL } from './data.js';
+import { getInvite } from './shared.js';
 
-const res = await fetch(
-  `${pdsURL}/xrpc/com.atproto.server.createInviteCode`,
-  {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${btoa('admin:hunter2')}`,
-      'Content-Type': 'application/json',
-    },
-    body: '{"useCount":1}',
-  }
-);
-if (res.status >= 400) {
-  console.warn(`Status: ${res.status} ${res.statusText}`);
-  console.warn(JSON.stringify(res.headers, null, 2));
-  console.warn(await res.text());
-}
-else {
-  const { code } = await res.json();
-  console.warn(`Invite: ${code}`);
-}
+await getInvite();
